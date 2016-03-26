cc9d67068300595fa3c929752247c13d58822f78
Taro Aoki <aizu.s1230022@gmail.com>
2016-03-27 07:11:33 +0900

    [Modify] Change base Activity

--- app/src/main/java/com/syfm/hxh/Activities/PersonalActivity.java
+++ app/src/main/java/com/syfm/hxh/Activities/PersonalActivity.java
@@ -1,5 +1,6 @@
 package com.syfm.hxh.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by lycoris on 2016/03/26.
@@ -24,6 +26,14 @@ public class PersonalActivity extends AppCompatActivity {
     Toolbar toolbar;
     @Bind(R.id.listview_personal)
     ListView listView;
+    @OnClick(R.id.button_next)
+    public void onClickNext() {
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+
+    private ArrayList<HashMap<String, String>> list = new ArrayList<>();
+    private static int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +46,21 @@ public class PersonalActivity extends AppCompatActivity {
         toolbar.setTitle("Personal");
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
 
-        // For debug
-
-        ArrayList<HashMap<String, String>> list = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            HashMap<String, String> data = new HashMap<>();
-            data.put(Const.QUESTION_NUMBER, String.format("Q%s.", String.valueOf(i+1)));
-            data.put(Const.QUESTION_TEXT,   "HogeHoge");
-            list.add(data);
-        }
+        setListData("深夜に電気を消してテレビを見るのが好きだ");
+        setListData("人混みの中にいるのが好きだ");
+        setListData("友達と釣りに行くのが好きだ");
 
         QuestionListAdapter adapter = new QuestionListAdapter(this, list);
         listView.setAdapter(adapter);
     }
 
 
+
+    private void setListData(String text) {
+        number++;
+        HashMap<String, String> data = new HashMap<>();
+        data.put(Const.QUESTION_NUMBER, String.format("Q%s.", String.valueOf(number)));
+        data.put(Const.QUESTION_TEXT,   text);
+        list.add(data);
+    }
 }
